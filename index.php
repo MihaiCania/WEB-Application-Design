a43e9b669cc0fe97d6c6617e4d9570329d8f30bc
MihaiCania <mihai.cania@gmail.com>
2017-10-25 08:45:48 +0300

    Add files via upload
    Main Interface

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="divCSS.css">
+</head>
+<body>
+
+<div class="header">
+	<p>This is the header</p>
+</div>
+
+<div class="left">
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+</div>
+
+<div class="main">
+  <h1>The City</h1>
+  <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+</div>
+
+<div class="right">
+	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+  	<div class="inside">
+  		<p>aaaaa</p>
+  	</div>
+
+</div>	
+
+
+</body>
+</html>
